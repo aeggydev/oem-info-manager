61e57a50ffe352d4bd7395cb3de44def9893e0bd
aeggydev <kucernak.a@gmail.com>
2021-03-19 21:24:20 +0100

    Implement whole UI and functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,99 @@
-﻿using System;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
+using System.Windows.Input;
 using Microsoft.Win32;
+using Size = System.Drawing.Size;
 
 namespace oem_logo
 {
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : INotifyPropertyChanged
     {
+        private readonly JsonSerializerOptions _jsonSerializerOptions = new()
+        {
+            WriteIndented = true
+        };
+
+        private ICommand _openPanelCommand;
+
+        private ICommand _openRegistryCommand;
+
+        private ICommand _undoImageCommand;
+
         public MainWindow()
         {
+            // TODO: Add a way to interact from the command line with arguments
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-us");
             InitializeComponent();
-            Console.WriteLine(SystemSettings.OemIcon);
-            Console.WriteLine(SystemSettings.OemIcon);
         }
+
+        public ICommand OpenRegistryCommand
+        {
+            get
+            {
+                if (_openRegistryCommand != null) return _openRegistryCommand;
+                _openRegistryCommand = new RelayCommand(_ =>
+                {
+                    Registry.SetValue(
+                        @"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Applets\Regedit",
+                        "LastKey",
+                        @"Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation");
+                    Process.Start("regedit.exe");
+                });
+                return _openRegistryCommand;
+            }
+        }
+
+        public ICommand OpenPanelCommand
+        {
+            get
+            {
+                if (_openPanelCommand != null) return _openPanelCommand;
+                _openPanelCommand = new RelayCommand(_ =>
+                {
+                    Process.Start("explorer", @"shell:::{BB06C0E4-D293-4f75-8A90-CB05B6477EEE}");
+                });
+
+                return _openPanelCommand;
+            }
+        }
+
+        public ICommand UndoImageCommand
+        {
+            get
+            {
+                if (_undoImageCommand != null) return _undoImageCommand;
+                _undoImageCommand = new RelayCommand(_ => { WindowModel.OemIcon.ResetCommand.Execute(null); },
+                    () => WindowModel.OemIcon.Changed);
+
+                return _undoImageCommand;
+            }
+        }
+
+        public SystemSettingsModel WindowModel { get; } = SystemSettingsModel.Instance();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         private void IconSelect_OnClick(object sender, RoutedEventArgs e)
         {
-            var dialog = new OpenFileDialog();
+            var dialog = new OpenFileDialog
+            {
+                Filter =
+                    "Image files (*.bmp;*.jpg;*.jpeg;*.png;)|*.bmp;*.BMP;*.jpg;*.JPG;*.jpeg;*.jpeg;*.png;*.PNG",
+                Title = "Select image",
+                CheckFileExists = true
+            };
+
             if (dialog.ShowDialog() != true) return;
             var file = new FileInfo(dialog.FileName);
             HandleFile(file);
@@ -30,49 +101,98 @@ namespace oem_logo
 
         private void HandleFile(FileInfo file)
         {
-            Status.Content = file.Name;
+            var programFilesPath = @"C:\Program Files\OEMChanger";
+            if (!Directory.Exists(programFilesPath)) Directory.CreateDirectory(programFilesPath);
+
             var extension = file.Extension.ToLower();
 
-            if (extension == ".bmp" || extension == ".png") // Bitmap image
+            string[] supportedFormats = {".bmp", ".png", ".jpg", ".jpeg"};
+            if (!supportedFormats.Contains(extension))
             {
-                var image = new Image {Source = new BitmapImage(new Uri(file.FullName))};
-                IconSelect.Content = image;
-                IconSetButton.IsEnabled = true;
+                var formatText = string.Join(", ", supportedFormats);
+                MessageBox.Show($"Image has to be one of the following formats: {formatText}", "Image format error");
+                return;
             }
+
+            var inputFile = file.FullName;
+            var outputFile = Path.Combine(programFilesPath, "logo.bmp");
+            if (File.Exists(outputFile)) File.Delete(outputFile);
+
+            var bitmap = new Bitmap(inputFile);
+            bitmap = new Bitmap(bitmap, new Size(120, 120));
+            bitmap.Save(outputFile, ImageFormat.Bmp);
+
+            WindowModel.OemIcon.LocalValue = outputFile;
         }
 
-        private void IconSelect_OnDragEnter(object sender, DragEventArgs e)
+        private void Import_OnClick(object sender, RoutedEventArgs e)
         {
-            IconSelect.Background = new SolidColorBrush(Colors.Bisque);
-        }
-
-        private void IconSelect_OnDragLeave(object sender, DragEventArgs e)
-        {
-            IconSelect.Background = new SolidColorBrush(Colors.Azure);
-        }
-
-        private void IconSelect_OnDragOver(object sender, DragEventArgs e)
-        {
-        }
-
-        private void IconSelect_OnDrop(object sender, DragEventArgs e)
-        {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            var dialog = new OpenFileDialog
             {
-                var data = (string[]) e.Data.GetData(DataFormats.FileDrop);
-                if (data?.Length > 1) // Multiple files dragged
-                {
-                    var error = MessageBox.Show("You can only drag one file.", "Error");
-                }
+                Title = "Open backup",
+                DefaultExt = "bak",
+                Filter = "backup files (*.bak)|*.bak|All files (*.*)|*.*",
+                CheckPathExists = true,
+                CheckFileExists = true
+            };
 
-                var file = new FileInfo(data[0]);
-                HandleFile(file);
+            var result = dialog.ShowDialog();
+            if (result != true) return;
+
+            var inFile = dialog.FileName;
+            var json = File.ReadAllText(inFile);
+
+            SettingsObject data;
+            try
+            {
+                data = JsonSerializer.Deserialize<SettingsObject>(json);
             }
+            catch
+            {
+                MessageBox.Show("Not a valid backup file.", "Backup format error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            WindowModel.Manufacturer.LocalValue = data?.Manufacturer;
+            WindowModel.Model.LocalValue = data?.Model;
+            WindowModel.OemIcon.LocalValue = data?.OemIconPath;
+            WindowModel.SupportHours.LocalValue = data?.SupportHours;
+            WindowModel.SupportPhone.LocalValue = data?.SupportPhone;
+            WindowModel.SupportUrl.LocalValue = data?.SupportUrl;
         }
 
-        private void IconSetButton_OnClick(object sender, RoutedEventArgs e)
+        private void Export_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            var dialog = new SaveFileDialog
+            {
+                FileName = "oem.bak",
+                Title = "Save backup",
+                DefaultExt = "bak",
+                Filter = "backup files (*.bak)|*.bak|All files (*.*)|*.*",
+                CheckPathExists = true
+            };
+            var result = dialog.ShowDialog();
+            if (result != true) return;
+            var outPath = dialog.FileName;
+
+            SaveSettingsFile(outPath);
+        }
+
+        private void SaveSettingsFile(string outPath)
+        {
+            var data = new SettingsObject
+            {
+                OemIconPath = WindowModel.OemIcon.LocalValue,
+                Manufacturer = WindowModel.Manufacturer.LocalValue,
+                Model = WindowModel.Model.LocalValue,
+                SupportHours = WindowModel.SupportHours.LocalValue,
+                SupportPhone = WindowModel.SupportPhone.LocalValue,
+                SupportUrl = WindowModel.SupportUrl.LocalValue
+            };
+            var json = JsonSerializer.Serialize(data, _jsonSerializerOptions);
+
+            File.WriteAllText(outPath, json);
         }
     }
 }
---
+++ SystemSettingsModel.cs
@@ -0,0 +1,153 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Microsoft.Win32;
+using oem_logo.Annotations;
+
+namespace oem_logo
+{
+    // Used for import / export
+    public record SettingsObject
+    {
+        public string OemIconPath { get; init; }
+        public string Manufacturer { get; init; }
+        public string Model { get; init; }
+        public string SupportHours { get; init; }
+        public string SupportPhone { get; init; }
+        public string SupportUrl { get; init; }
+    }
+
+    public class SystemSettingsModel
+    {
+        private const string KeyName = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation";
+        private static readonly SystemSettingsModel _systemSettingsModel = new();
+
+        private ICommand _setCommand;
+
+        private ICommand _undoFieldsCommand;
+
+        private SystemSettingsModel()
+        {
+            FieldPropList = new[] {Manufacturer, Model, SupportHours, SupportPhone, SupportUrl};
+            PropList = new[] {OemIcon, Manufacturer, Model, SupportHours, SupportPhone, SupportUrl};
+        }
+
+        public ICommand SetCommand
+        {
+            get
+            {
+                if (_setCommand != null) return _setCommand;
+                _setCommand = new RelayCommand(_ =>
+                {
+                    foreach (var prop in PropList) prop.SetToLocal();
+                }, () => PropList.Any(x => x.Changed));
+                return _setCommand;
+            }
+        }
+
+        public ICommand UndoFieldsCommand
+        {
+            get
+            {
+                if (_undoFieldsCommand != null) return _undoFieldsCommand;
+                _undoFieldsCommand = new RelayCommand(_ =>
+                {
+                    foreach (var prop in FieldPropList) prop.ResetCommand.Execute(null);
+                }, () => PropList.Any(x => x.Changed));
+                return _undoFieldsCommand;
+            }
+        }
+
+        public ImageProp OemIcon { get; } = new("Logo");
+        public Prop Manufacturer { get; } = new("Manufacturer");
+        public Prop Model { get; } = new("Model");
+        public Prop SupportHours { get; } = new("SupportHours");
+        public Prop SupportPhone { get; } = new("SupportPhone");
+        public Prop SupportUrl { get; } = new("SupportURL");
+        public Prop[] FieldPropList { get; }
+        public Prop[] PropList { get; }
+
+        public static SystemSettingsModel Instance()
+        {
+            return _systemSettingsModel;
+        }
+
+        public class ImageProp : Prop
+        {
+            public ImageProp(string valueName) : base(valueName)
+            {
+            }
+
+            public string Filename => new FileInfo(LocalValue).Name;
+        }
+
+        public class Prop : INotifyPropertyChanged
+        {
+            private string _localValue;
+
+            private ICommand _resetCommand;
+
+            public Prop(string valueName)
+            {
+                ValueName = valueName;
+                LocalValue = SystemValue;
+            }
+
+            public string ValueName { get; }
+
+            public ICommand ResetCommand
+            {
+                get
+                {
+                    if (_resetCommand != null) return _resetCommand;
+                    _resetCommand = new RelayCommand(_ => { LocalValue = SystemValue; }, () => Changed);
+
+                    return _resetCommand;
+                }
+            }
+
+            public string SystemValue
+            {
+                get => Registry.GetValue(KeyName, ValueName, "") as string;
+                set
+                {
+                    Registry.SetValue(KeyName, ValueName, value);
+                    OnPropertyChanged(nameof(Changed));
+                    OnPropertyChanged(nameof(SystemValue));
+                }
+            }
+
+            public bool Changed => !SystemValue.Equals(LocalValue);
+
+            public string LocalValue
+            {
+                get => _localValue;
+                set
+                {
+                    _localValue = value;
+                    OnPropertyChanged(nameof(LocalValue));
+                    OnPropertyChanged(nameof(Changed));
+                    OnPropertyChanged("CanSet");
+
+                    // TODO: Move to ImageProp definition
+                    OnPropertyChanged("Filename");
+                }
+            }
+
+            public event PropertyChangedEventHandler? PropertyChanged;
+
+            public void SetToLocal()
+            {
+                SystemValue = LocalValue;
+            }
+
+            [NotifyPropertyChangedInvocator]
+            private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
